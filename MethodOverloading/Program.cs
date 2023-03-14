1bc4ebb28c160c3bff289e34e946b6fdb908cba8
daisy.omoikirodah <thesmexxiichic@gmail.com>
2023-03-13 20:39:50 -0400

    done

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,38 @@
 {
     public class Program
     {
+        public static int Add(int num1, int num2)
+        {
+            return (num1 + num2);
+
+            
+        }
+        public static decimal Add(decimal num1,decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1,int num2,bool isTrue) 
+        {
+            var sum = num1 + num2;
+            if (isTrue && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+             else if (isTrue && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+              else
+            {
+                return sum.ToString();
+            }
+         
+
+        
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
         }
-    }
+    } 
 }
